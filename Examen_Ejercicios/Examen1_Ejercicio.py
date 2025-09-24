0ee27130a9b5af3496a41231367f591272a43b22
Abigail Cetina <abigail.cetina.ro@gmail.com>
2025-09-24 17:38:49 -0600

    Update Examen1_Ejercicio.py

--- Examen_Ejercicios/Examen1_Ejercicio.py
+++ Examen_Ejercicios/Examen1_Ejercicio.py
@@ -1,14 +1,3 @@
-"""4. Realizar un programa con el siguiente menú:
-      1. Llenar Vector A de manera aleatoria.
-      2. Llenar Vector B de manera aleatoria.
-      3. Realizar C=A+B
-      4. Realizar C=B-A
-      5. Mostrar (Permitiendo al usuario elegir entre el Vector A, B, o C).
-      6. Salir.
-
-NOTA.
-El rango de los números aleatorios para los Vectores será de [-100 a 100]. Validar opción 3, 4 y mostrar C de 5.
-La longitud de los Vectores es la misma, por lo tanto, solo se solicitará una vez."""
 
 import random
 
@@ -117,3 +106,4 @@ def menu():
 
 if __name__ == "__main__":
     menu()
+
